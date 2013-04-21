a3ab346c0d0d88a464a78719b968875e15cb50eb
Hike Danakian <hike@hdanak.com>
2013-04-20 18:17:57 -0700

    initial commit

---
+++ lib/List/Convolve.pm
@@ -0,0 +1,12 @@
+package List::Convolve;
+our $VERSION = v0.0.1;
+
+use Modern::Perl;
+
+=head1 SYNOPSIS
+
+    ...
+
+=cut
+
+1
---
+++ t/001-load.t
@@ -0,0 +1,8 @@
+use Modern::Perl;
+use Test::More;
+
+BEGIN {
+    require_ok 'List::Convolve';
+}
+
+done_testing 1
