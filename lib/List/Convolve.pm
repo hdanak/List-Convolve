58842a8b08af9d0ad5cd217e901fc5ecd7127adb
Hike Danakian <hike@hdanak.com>
2013-05-11 22:39:00 -0700

    fixed, simpler but maybe less efficient

--- lib/List/Convolve.pm
+++ lib/List/Convolve.pm
@@ -26,54 +26,26 @@ sub convolve_array {
     return [] unless @$window;
     croak "Window cannot be larger than list" if @$window > @$list;
     croak "Window size must be odd, or a center must be specified. "
-            . "Try inserting 0 to the beginning or end"
+            . "Try inserting 0 at the beginning or end"
         unless @$window % 2 or exists $opts{center};
 
-    convolve_hash({map {$_ => $$window[$_]} 0..$#$window}, $list, %opts);
-
-    my $mapper  = $opts{mapper}  // sub { $_[0] * $_[1]};
-    my $reducer = $opts{reducer} // sub { $_[0] + $_[1] };
-    my $center  = $opts{center}  // @$window/2;
-
-    my @out = ([]) x @$list;
-    for my $i(0 .. $center - 1) {
-        push @{$out[$i]}, map {
-            &$mapper($$window[$center + $_], $$list[$i + $_])
-        } (-$i .. $#$window - $center)
-    }
-    for my $i($center .. $#$list + $center - $#$window) {
-        push @{$out[$i]}, map {
-            &$mapper($$window[$center + $_], $$list[$i + $_])
-        } (-$center .. $#$window - $center)
-    }
-    for my $i(1 + $#$list + $center - $#$window .. $#$list) {
-        push @{$out[$i]}, map {
-            &$mapper($$window[$center + $_], $$list[$i + $_])
-        } (-$center .. $#$list - $i)
-    }
-    [ map {
-        my $prev = shift @$_;
-        for (@$_) {
-            $prev = &$reducer($prev, $_)
-        }
-        $prev
-    } @out ]
+    my $center  = $opts{center}  // int(@$window/2);
+    convolve_hash({map {$_ - $center => $$window[$_]} 0..$#$window}, $list, %opts);
 }
 sub convolve_hash {
     my ($kern, $list, %opts) = @_;
     my $mapper  = $opts{mapper}  // sub { $_[0] * $_[1]};
     my $reducer = $opts{reducer} // sub { $_[0] + $_[1]};
+    my $default = $opts{default};
 
     my @keys = sort keys %$kern;
+    return [] unless @keys;
     [ map {
         my $i = $_;
-
         my @xs = map { &$mapper($$kern{$_}, $$list[$i + $_]) }
-                grep { $i + $_ > 0 and $i + $_ <= $#$list } @keys;
+                grep { $i + $_ >= 0 and $i + $_ <= $#$list } @keys;
 
-        use Data::Dumper;
-        say Dumper \@xs;
-        my $prev = shift @xs;
+        my $prev = shift @xs // $default;
         $prev = &$reducer($prev, $_) for @xs;
         $prev
     } 0..$#$list ]
--- t/ConvolveArray.t
+++ t/ConvolveArray.t
@@ -3,15 +3,15 @@ use Test::More;
 
 use List::Convolve qw/convolve/;
 
-is_deeply convolve([1,3,1], [0..5]), [1,5,9,15,20,19];
+is_deeply convolve([1,3,1], [0..5]), [1,5,10,15,20,19];
 ok not eval { convolve([1,3,1,0], [0..5]) };
-is_deeply convolve([0,1,3,1], [0..5], center => 2), [1,5,9,15,20,19];
+is_deeply convolve([0,1,3,1], [0..5], center => 2), [1,5,10,15,20,19];
 
-is_deeply convolve([1,3,1], [0..5], (
+is_deeply convolve([1,3,1], [0..5],
     mapper  => sub { $_[0] + $_[1] },
     reducer => sub { $_[0] * $_[1] },
 #   wrap    => 'cycle',
-)), [6, 12, 40, 90, 168, 40];
+), [6, 12, 40, 90, 168, 40];
 
 ok not eval { convolve([1,3,1], []) };
 
